497831cc5e6af386e9003c18dc20b280ffba880e
sl2000stat <simon.leiner1@gmail.com>
2023-05-27 22:31:56 +0200

    inital commit

--- pytorch_helper_functions.py
+++ pytorch_helper_functions.py
@@ -9,6 +9,10 @@ import numpy as np
 import torch
 
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Checked: Function works
+
 def set_global_seed(seed=42):
     """
     This function sets the global seed for all random number generators.
@@ -23,3 +27,29 @@ def set_global_seed(seed=42):
     torch.cuda.manual_seed(seed)
 
     return None
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Checked: Function works
+
+def print_train_time(start, end, device=None):
+    """
+    This function prints the time it took to train the model.
+
+    :param start: float: Start time of computation (preferred in timeit format).
+    :param end: float: End time of computation.
+    :param device: str: Device that compute is running on. Defaults to None.
+    :return: total_time: float: time between start and end in seconds (higher is longer).
+    """
+
+    # calculate the total time
+    total_time = end - start
+
+    # print the total time
+    print(f"\nTrain time on {device}: {total_time:.3f} seconds")
+
+    return total_time
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
